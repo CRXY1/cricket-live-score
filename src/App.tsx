897ac50c8c9335795471a8ecc98adfa77f99d0fe
CRXY1 <syedfamilyof5@gmail.com>
2025-07-23 23:39:44 -0400

    Added NEWS Section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import LiveScores from './pages/LiveScores';
 import Series from './pages/Series';
 import Teams from './pages/Teams';
+import News from './pages/News';
 
 const App: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const App: React.FC = () => {
           <Route path="/live-scores" element={<LiveScores />} />
           <Route path="/series" element={<Series />} />
           <Route path="/teams" element={<Teams />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </div>
     </Router>
